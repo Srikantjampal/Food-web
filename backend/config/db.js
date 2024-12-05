6dcf059b8563c7129fda11a10176f8a1e14c4241
Srikantjampal <srikant.jampal.12@gmail.com>
2024-12-05 21:51:35 +0530

    pull

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () =>{
+<<<<<<< HEAD
     await mongoose.connect('mongodb+srv://Srijamp30:4eb426d8@cluster0.a8keswg.mongodb.net/Food-web').then(()=>console.log('DB is Connected'));
+=======
+    await mongoose.connect('mongodb+srv://Srijamp30:Srijamp30@cluster0.a8keswg.mongodb.net/Food-web').then(()=>console.log('DB is Connected'));
+>>>>>>> 39209a7 (pull)
 }
